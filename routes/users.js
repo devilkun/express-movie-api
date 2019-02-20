0e6aad03545204cd02581e8e8aea580a53bc3f1b
devilkun <zwy876910200@qq.com>
2019-02-20 17:59:05 +0800

    找回密码接口

--- routes/users.js
+++ routes/users.js
@@ -73,6 +73,69 @@ router.post('/support',function(req,res,next){
 });
 //用户找回密码
 router.post('/findPassword',function(req,res,next){
+//需要输入用户的邮箱和手机信息,同时可以更新密码
+//这里需要两个返回情况,一个是req.body.repassword存在时,另一个是req.repassword不存在时
+//这个接口同时用于密码的重置,需要用户登录
+  if(req.body.repassword){
+    //当存在时,需要验证其登录登录情况或者验证其code
+    if(req.body.token){
+      //当存在code登录状态时,验证其状态
+      if(!req.body.user_id){
+        res.json({status:0,message:'用户登录错误'});
+      }
+      if(!req.body.password){
+        res.json({status:0,message:'用户旧密码错误'})
+      }
+      if(req.body.token == getMD5Password(req.body.user_id)){
+         user.findOne({_id:req.body.user_id,password:req.body.password},function(err,checkUser){
+            if(checkUser){
+              user.update({_id:req.body.user_id},{password:req.body.repassword},function(err,userUpdate){
+                if(err){
+                  res.json({status:0,message:'更改错误',data:err})
+                }
+                res.json({status:1,message:'更改成功',data:userUpdate})
+              })
+            }else{
+              res.json({status:0,message:'用户旧密码错误'});
+            }
+        });
+      }else{
+        res.json({status:0,message:'用户登录错误'});
+      }
+    }else{
+      //不存在code时,直接验证mail和phone
+      user.findUserPassword(req.body.username,req.body.userMail,req.body.userPhone,function(err,userFound){
+        if(userFound.length != 0){
+          user.update({_id:userFound[0]._id},{password:req.body.repassword},function(err,userUpdate){
+            if(err){
+              res.json({status:0,message:'更新错误',data:err})
+            }
+            res.json({status:1,message:'更新成功',data:userUpdate});  
+          });
+        }else{
+          res.json({status:0,message:'信息错误'})
+        }
+      })
+    }
+  }else{
+    //这里只是验证mail和phone,返回验证成功提示和提交的字段,用于之后的改密码操作
+    if(!req.body.username){
+      res.json({status:0,message:'用户名称为空'});
+    }
+    if(!req.body.userMail){
+      res.json({status:0,message:'用户邮箱为空'});
+    }
+    if(!req.body.userPhone){
+      res.json({status:0,message:'用户手机为空'});
+    }
+    user.findUserPassword(req.body.username,req.body.userMail,req.body.userPhone,function(err,userFound){
+      if(userFound.length != 0){
+        res.json({status:1,message:'验证成功,请修改密码',data:{userMail:req.body.userMail,userPhone:req.body.userPhone}})
+      }else{
+        res.json({status:0,message:'信息错误'});
+      }
+    })
+  }
 });
 //用户发送站内信
 router.post('/sendEmail',function(req,res,next){
